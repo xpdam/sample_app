2f95777cd302c18ad480f606edd07ee002b29002
Marco Dallagiacoma <marco.dallagiacoma@xpeppers.com>
2018-07-12 15:53:11 +0200

    Complete static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-
-  root to: 'application#hello'
 end
