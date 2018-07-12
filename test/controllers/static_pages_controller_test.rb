0d095ba92d254560d62a1beb439f4141ec358f64
Marco Dallagiacoma <marco.dallagiacoma@xpeppers.com>
2018-07-12 16:40:45 +0200

    Add helper for dynamic title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,8 +8,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
-
 
   test "should get home" do
     get static_pages_home_url
