51fd880f9f8bfa59912d4549f4949269fee8aa1f
Marco Dallagiacoma <marco.dallagiacoma@xpeppers.com>
2018-07-17 16:21:27 +0200

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -19,4 +22,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
